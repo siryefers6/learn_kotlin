c6d4ece18c5c24f73872d36c375804f16ad9545e
yeferson <siryefers6@gmail.com>
2025-05-14 00:21:48 +0000

    feat: Implementar clase Monstruo

---
+++ app/src/main/kotlin/org/example/Monstruo.kt
@@ -0,0 +1,11 @@
+package org.example
+
+class Monstruo(
+    val nombre: String,
+    val tipo: String,
+    val habitats: List<String>,
+    val debilidadElemental: List<String>,
+    val materialesRangoBajo: List<String>,
+    val materialesRangoAlto: List<String>
+)
+
